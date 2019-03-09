c37eba94bc44efef0adcc36db90f8c2e1d1aeca3
jensakrom <jensakrom@gmail.com>
2019-03-10 05:58:32 +0700

    serialize & deserialize User

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const app = express();
 const authRoutes = require('./routes/auth-routes');
-const passportSetup = require('./config/passport-setup')
+const passportSetup = require('./config/passport-setup');
+const keys = require('./config/keys');
+const mongoose = require('mongoose');
 
 
 // Setup up view engine
 app.set('view engine', 'ejs');
+
+// connect mongodb
+mongoose.connect(keys.mongodb.DbURI, {useNewUrlParser: true}, (err) => {
+    if(!err){
+        console.log('Mongodb connection succed');
+    }else {
+        console.log('Error in Mongodb Connection.')
+    }
+})
 
 // Setup Routes
 app.use('/auth',authRoutes);
--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,16 +1,38 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys');
+const User = require('../models/user-model');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then((user) => {
+        done(null, user);
+    });
+});
 
 passport.use(
     new GoogleStrategy({
         // option for the google starategy
         clientID: keys.google.clientId,
         clientSecret: keys.google.clientSecret,
-        callbackURL:'/auth/google/redirect'
 }, (accessToken, refreshToken, profile, done) => {
-    // passport callback function
-    console.log('Passport Callback function fire');
-    console.log(profile)
+    // Check if id is exist
+    User.findOne({googleId: profile.id}).then((currentUser) => {
+        if(currentUser){
+            console.log('Id its Exist')
+            done(null, currentUser);
+        }else{
+            new User({
+                username: profile.displayName,
+                googleId: profile.id
+            }).save().then((newUser) => {
+                console.log('new User created ' + newUser )
+                done(null, newUser)
+            })
+        }
+    })
 })
 );
